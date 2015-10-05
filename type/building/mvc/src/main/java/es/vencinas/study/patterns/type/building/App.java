79d4aac618ca78f80905092a3ca0a944991051f1
Valentín Encinas Rojas <vencinas@atsistemas.com>
2015-10-05 17:29:01 +0200

    Finished Design Pattern

---
+++ type/building/mvc/src/main/java/es/vencinas/study/patterns/type/building/App.java
@@ -0,0 +1,13 @@
+package es.vencinas.study.patterns.type.building;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ type/building/singleton/src/main/java/es/vencinas/study/pattterns/type/building/singleton/SingletonEagerInitialization.java
@@ -0,0 +1,36 @@
+package es.vencinas.study.pattterns.type.building.singleton;
+
+/**
+ * The eager initialization singleton creates a new INSTANCE when the class is
+ * initialized.
+ * <p>
+ * An eager initialization is the simplest way to implement this design pattern.
+ * Only use this implementation if the cost of creating the INSTANCE is not too
+ * large in terms of time/resources.
+ * <p>
+ *
+ * @version 1.0.0 - 01 Oct 2016
+ * @author Valentin Encinas
+ */
+public final class SingletonEagerInitialization {
+    /**
+     * A {@code static final} INSTANCE of this class.
+     */
+    private static final SingletonEagerInitialization INSTANCE = new SingletonEagerInitialization();
+
+    /**
+     * Return an INSTANCE of {@code SingletonEagerInitialization}.
+     *
+     * @return A reference to {@code SingletonEagerInitialization}
+     */
+    public static SingletonEagerInitialization getInstance() {
+        return SingletonEagerInitialization.INSTANCE;
+    }
+
+    /**
+     * A {@code private} default constructor to ensure
+     * {@code SingletonEagerInitialization} cannot be called by other classes.
+     */
+    private SingletonEagerInitialization() {
+    }
+}
